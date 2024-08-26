e92a75c1e7c183bf789a828afd4f1d0aaa751d3d
gbhem <gbhem@BLAKESPC>
2024-08-25 19:16:40 -0500

    Add project files.

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your first name");
+
+string firstname = Console.ReadLine();
+
+Console.WriteLine("What is your last name");
+
+string lastname = Console.ReadLine();
+
+Console.WriteLine("What is your age");
+
+string age = Console.ReadLine();
+
+Console.WriteLine("What is your favorite color");
+
+string color = Console.ReadLine();
+
+Console.WriteLine("Hello, " + firstname + " " + lastname + ("! You are " + age + (" years old and your favorite color is ") + color));
+
+Console.Read();
+
